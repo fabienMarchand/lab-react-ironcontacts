a0b95dff8bf88aee63890bee4f9e5995c63911f3
Marcha <marchaf@gmail.com>
2020-10-06 17:53:58 +0200

    Css ultra pop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import contactJson from './contacts.json';
 
@@ -65,9 +65,7 @@ class App extends React.Component {
           </tr>
         </thead>
         <tbody>
-
         {this.renderContact()}
-        
         </tbody>
       </table>
     </div>
